8e78e7949c6873e333b5fdc23be7ab80b642d0bb
ismay <hello+github@ismaywolff.nl>
2017-08-16 13:25:11 +0200

    Update polyfill (#633)
    * Update polyfill
    
    * Simplify polyfill
    
    * Remove some snapshots

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,14 +1,10 @@
-/* eslint-disable no-underscore-dangle, no-mixed-operators */
-
-import load from 'load-script'
 import React from 'react'
 import { render } from 'react-dom'
 import createHistory from 'history/createBrowserHistory'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime'
-import { logError } from '../shared/services/raven'
 import configureStore from '../shared/store'
 import { getState } from '../shared/services/persist'
-import { App, AppWithErrors } from './components/app'
+import { App } from './components/app'
 import { init } from './services/raven'
 
 /**
@@ -34,36 +30,11 @@ OfflinePluginRuntime.install({
 })
 
 /**
- * Boots the app, shows errors if there were any
+ * Boots the app
  */
 
-const boot = error => {
-  if (error) {
-    logError(error)
-    render(<AppWithErrors errorMessage={error.message} />, document.getElementById('app'))
-  } else {
-    const history = createHistory()
-    const store = configureStore(getState())
+const history = createHistory()
+const store = configureStore(getState())
 
-    import('./services/analytics').then(analytics => analytics.init(history))
-    render(<App store={store} history={history} />, document.getElementById('app'))
-  }
-}
-
-/**
- * Feature tests
- */
-
-const hasFetch = 'fetch' in window
-const hasPromise = 'Promise' in window
-const hasObjectAssign = typeof Object.assign === 'function'
-
-/**
- * Checks if the client supports all necessary features, and polyfills them if necessary
- */
-
-if (hasFetch && hasPromise && hasObjectAssign) {
-  boot()
-} else {
-  load('https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,Object.assign,Promise', boot)
-}
+import('./services/analytics').then(analytics => analytics.init(history))
+render(<App store={store} history={history} />, document.getElementById('app'))
