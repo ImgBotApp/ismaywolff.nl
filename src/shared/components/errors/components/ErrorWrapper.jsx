a650c71880d06fbb6944beb87a14c4a504318074
ismay <hello+github@ismaywolff.nl>
2017-08-24 12:24:51 +0200

    Remove unused default cases (#642)

--- src/shared/components/errors/components/ErrorWrapper.jsx
+++ src/shared/components/errors/components/ErrorWrapper.jsx
@@ -1,27 +1,23 @@
+/* eslint-disable default-case, consistent-return */
+
 import { oneOf } from 'prop-types'
 import styled from 'styled-components'
 
-/* istanbul ignore next: default case cannot be tested without triggering a proptype warning */
 const setBackground = props => {
   switch (props.type) {
     case 'info':
       return 'var(--color-blue-light)'
     case 'error':
       return 'var(--color-red-light)'
-    default:
-      return 'var(--color-white)'
   }
 }
 
-/* istanbul ignore next: default case cannot be tested without triggering a proptype warning */
 const setColor = props => {
   switch (props.type) {
     case 'info':
       return 'var(--color-blue-dark)'
     case 'error':
       return 'var(--color-red-dark)'
-    default:
-      return 'var(--color-black)'
   }
 }
 
