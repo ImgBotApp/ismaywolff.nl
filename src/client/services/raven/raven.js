42fed78fc764059170a48bd232fb502b2b1ac4cb
ismay <hello+github@ismaywolff.nl>
2017-08-28 12:58:19 +0200

    Ignore uncaught errors (#649)

--- src/client/services/raven/raven.js
+++ src/client/services/raven/raven.js
@@ -1,11 +1,10 @@
 /* global Raven */
 
 import load from 'load-script'
-import { __ERROR_EVENTS__ } from '../../../shared/constants'
-import { url, config, logError } from '../../../shared/services/raven'
+import { url, config } from '../../../shared/services/raven'
 
 /**
- * Initialize error tracking in production and log uncaught errors
+ * Initialize error tracking in production
  */
 
 const init = () => {
@@ -13,13 +12,6 @@ const init = () => {
     load('https://cdn.ravenjs.com/3.17.0/raven.min.js', () => {
       if (typeof Raven !== 'undefined' && 'config' in Raven) {
         Raven.config(url, config).install()
-      }
-
-      if (__ERROR_EVENTS__ in window) {
-        const errorEvents = window[__ERROR_EVENTS__].q || []
-        errorEvents.map(event => logError(event.error))
-        removeEventListener('error', window[__ERROR_EVENTS__])
-        delete window[__ERROR_EVENTS__]
       }
     })
   }
--- src/server/templates/catchErrors.js
+++
@@ -1,9 +0,0 @@
-import { __ERROR_EVENTS__ } from '../../shared/constants'
-
-const catchErrors = `
-  <script>
-    addEventListener('error', window.${__ERROR_EVENTS__}=function f(e){f.q=f.q||[];f.q.push(e)});
-  </script>
-`
-
-export default catchErrors
