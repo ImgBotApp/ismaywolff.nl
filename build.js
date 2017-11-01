de9c2bb360db5e03552716cccfaf6347ed27df4d
ismay <ismay@ismaywolff.nl>
2017-11-01 09:54:05 +0100

    Minify html (#702)

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 const metalsmith = require('metalsmith');
 const inPlace = require('metalsmith-in-place');
+const htmlMinifier = require('metalsmith-html-minifier');
 const metadata = require('./metadata');
 
 metalsmith(__dirname)
   .metadata(metadata)
   .use(inPlace())
+  .use(htmlMinifier())
   .build((err) => {
     if (err) {
       throw err;
